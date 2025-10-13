6062a809132cc7a5c4a6233eb3b3b67562b4278a
rodrigo11122 <rodrigorivera22255@gmail.com>
2025-10-13 01:02:33 -0500

    Create Ejercicio2_NuevasFuncionalidades.java

---
+++ Ejercicios_Guia07_Java/Ejercicio2_NuevasFuncionalidades.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Ejercicio2_NuevasFuncionalidades extends Ejercicio1_GestorPersonajes {
+    // Este ejemplo se integraría al Gestor principal para filtrar, ordenar y mostrar estadísticas.
+    // Se dejan comentarios para indicar mejoras.
+
+    // Posibles nuevas funciones:
+    // 1. Filtrar personajes por atributo (vida, ataque, etc.)
+    // 2. Mostrar estadísticas promedio.
+    // 3. Importar personajes desde otro archivo.
+}
